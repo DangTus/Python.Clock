b1c91506c83203c79ebba7b1b247ffdb7ecd23bd
DangTus <dangtu9520@gmail.com>
2022-03-20 22:24:26 +0700

    Xong, đang hoàn thiện

--- index.py
+++ index.py
@@ -1,14 +1,37 @@
 import pygame
 import time
+import math
 
 def draw_function():
+	# vẽ 2 dấu cộng, start
 	pygame.draw.rect(screen, WHILE, (100, 50, 50, 50))
 	pygame.draw.rect(screen, WHILE, (200, 50, 50, 50))
 	pygame.draw.rect(screen, WHILE, (300, 50, 150, 50))
 
+	# vẽ 2 dấu trừ, reset
 	pygame.draw.rect(screen, WHILE, (100, 200, 50, 50))
 	pygame.draw.rect(screen, WHILE, (200, 200, 50, 50))
 	pygame.draw.rect(screen, WHILE, (300, 200, 150, 50))
+
+# vẽ đồng hồ
+def draw_clock(time, tamX, tamY, r, lenKimPhut, lenKimGiay, speed):
+	pygame.draw.circle(screen, WHILE, (tamX, tamY), r) # vẽ mặt đồng hồ
+	pygame.draw.circle(screen, BLACK, (tamX, tamY), 5) # vẽ tâm đồng hồ
+
+	#vẽ kim đồng hồ
+	minute = int((time/speed)/60)
+	second = time - minute*(60*speed)
+	minute_alpa = float(minute) + second/(60*speed)
+
+	# vẽ kim phút
+	x = tamX + lenKimPhut*math.sin((minute_alpa*math.pi)/30)
+	y = tamY - lenKimPhut*math.cos((minute_alpa*math.pi)/30)
+	pygame.draw.line(screen, BLACK, (tamX, tamY), (x, y))
+
+	# vẽ kim giây
+	x = tamX + lenKimGiay*math.sin((second*math.pi)/(30*speed))
+	y = tamY - lenKimGiay*math.cos((second*math.pi)/(30*speed))
+	pygame.draw.line(screen, RED, (tamX, tamY), (x, y))
 
 def setText_function():
 	screen.blit(text_plus, (100, 50))
@@ -18,22 +41,43 @@ def setText_function():
 	screen.blit(text_start, (300, 50))
 	screen.blit(text_reset, (300, 200))
 
-def setTime_function(total):
-	text_min = font.render(str(int(total/60)), True, BLACK)
+def setTime_function(time, speed):
+	minute = int(time/60/speed)
+	second = (time - minute*60*speed)/speed
+
+	if second % 1 != 0:
+		if second + 1 >= 60:
+			second = 0
+			minute += 1
+		else:
+			second += 1
+
+	text_min = font.render(str(minute), True, BLACK)
 	text_colon = font.render(":", True, BLACK)
-	text_sec = font.render(str(total-int(total/60)*60), True, BLACK)
+	text_sec = font.render(str(int(second)), True, BLACK)
 
 	screen.blit(text_min, (100, 125))
 	screen.blit(text_colon, (160, 125))
 	screen.blit(text_sec, (200, 125))
 
+def draw_thanh_thoi_gian(time, total):
+	if total == 0:
+		redLen = 0	
+	else:
+		redLen = (time/total)*394
+	pygame.draw.rect(screen, BLACK, (50, 550, 400, 50))
+	pygame.draw.rect(screen, WHILE, (53, 553, 394, 44))
+	pygame.draw.rect(screen, RED, (53, 553, redLen, 44))
+
 pygame.init()
 
-screen = pygame.display.set_mode((500, 600))
+screen = pygame.display.set_mode((500, 650))
+pygame.display.set_caption('Đồng hồ đếm ngược')
 
 GREY = (120, 120, 120)
 BLACK = (0, 0, 0)
 WHILE = (255, 255, 255)
+RED = (255, 0, 0)
 
 font = pygame.font.SysFont('sans', 25)
 
@@ -43,7 +87,10 @@ text_start = font.render("Start", True, BLACK)
 text_reset = font.render("Reset", True, BLACK)
 
 running = True
+status = False
 total = 0
+remaining = 0
+speed = 100
 
 while running:
 	screen.fill(GREY)
@@ -54,7 +101,22 @@ while running:
 
 	setText_function()
 
-	setTime_function(total)
+	# draw_clock(thời gian(sec), tâm X, tâm Y, bán kính, len kim dài, len kim ngắn, tốc độ)
+	draw_clock(remaining, 250, 400, 100, 90, 90, speed)
+
+	draw_thanh_thoi_gian(remaining, total * speed)
+	
+	setTime_function(remaining, speed)		
+
+	if status:
+		if remaining < 0:
+			total = 0
+			status = False
+		else:
+			remaining = remaining - 1
+			time.sleep(1/speed)
+	else:
+		remaining = total * speed
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -63,19 +125,32 @@ while running:
 			if event.button == 1:
 				#reset button
 				if mouseX >= 300 and mouseX <= 450 and mouseY >= 200 and mouseY <= 250:
+					status = False
 					total = 0
+
+				#start button
+				elif mouseX >= 300 and mouseX <= 450 and mouseY >= 50 and mouseY <= 100:
+					status = True
+
 				#plus min button
-				if mouseX >= 100 and mouseX <= 150 and mouseY >= 50 and mouseY <= 100:
-					total = total + 60
+				elif mouseX >= 100 and mouseX <= 150 and mouseY >= 50 and mouseY <= 100:
+					if not status:
+						total = total + 60
+
 				#minus min button
-				if mouseX >= 100 and mouseX <= 150 and mouseY >= 200 and mouseY <= 250:
-					total = total - 60 if total >= 60 else 0
+				elif mouseX >= 100 and mouseX <= 150 and mouseY >= 200 and mouseY <= 250:
+					if not status:
+						total = total - 60 if total >= 60 else 0
+
 				#plus sec button
-				if mouseX >= 200 and mouseX <= 250 and mouseY >= 50 and mouseY <= 100:
-					total = total + 1
+				elif mouseX >= 200 and mouseX <= 250 and mouseY >= 50 and mouseY <= 100:
+					if not status:
+						total = total + 1
+
 				#minus sec button
-				if mouseX >= 200 and mouseX <= 250 and mouseY >= 200 and mouseY <= 250:
-					total = total - 1 if total >= 1 else 0
+				elif mouseX >= 200 and mouseX <= 250 and mouseY >= 200 and mouseY <= 250:
+					if not status:
+						total = total - 1 if total >= 1 else 0
 
 	pygame.display.flip()
 
--- test.py
+++ test.py
@@ -1,20 +1,49 @@
-# import time
+import pygame
+import time
+import math
 
-# i=1
+pygame.init()
 
-# while True:
+screen = pygame.display.set_mode((500, 650))
 
-# 	print(i)
-# 	i = i+1
-# 	time.sleep(2)
+GREY = (120, 120, 120)
+BLACK = (0, 0, 0)
+WHILE = (255, 255, 255)
+RED = (255, 0, 0)
 
-# 	if i == 10:
-# 		break
+font = pygame.font.SysFont('sans', 25)
 
-def hello1(**var):
-	print(var["a"])
+running = True
+start_status = False
+total = 0
+time_start = 0
 
-a = "hello1"
-b = "hello2"
+while running:
+	screen.fill(GREY)
 
-hello1(b = b, a = a)
+	if not start_status:
+		pygame.draw.rect(screen, WHILE, (50, 550, 400, 50))
+		pygame.draw.rect(screen, RED, (50, 550, 400, 50))
+	else:
+		if i>=0:
+			if i % 100 == 0:
+				print(i/100)
+			redLen = (i/float(total))*400
+			pygame.draw.rect(screen, RED, (50, 550, redLen, 50))
+			pygame.draw.rect(screen, WHILE, (50+redLen, 550, 400-redLen, 50))			
+			time.sleep(0.01)
+			i = i-1
+		else:
+			start_status = False
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			running = False
+		if event.type == pygame.MOUSEBUTTONDOWN:
+			i = 100
+			total = 100
+			start_status = True
+
+	pygame.display.flip()
+
+pygame.quit()
